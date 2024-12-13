345edfd8c812dba5fd424d0d477362c37ee0d96a
Curro Abascal <francisco.abascal@ieo.csic.es>
2024-12-13 13:26:26 +0000

    another commit

---
+++ R/thermstruct.r
@@ -0,0 +1,16 @@
+#' Isocline depth by tag and days estimated from the archival data (function get.thermalstruct)
+#'
+#' @docType data
+#'
+#' @usage data(thermstruct)
+#'
+#' @format An object of class \code{"data.frame"}
+#'
+#' @keywords datasets
+#' @examples
+#' data(thermstruct)
+#' head(thermstruct)
+#' 
+
+
+"thermstruct"
