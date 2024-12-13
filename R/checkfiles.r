98bd148b2ef4121a608332c95b2fa425fe7b11d9
Curro Abascal <francisco.abascal@ieo.csic.es>
2024-12-13 14:23:39 +0000

    initial commit

--- R/checkfiles.r
+++ R/checkfiles.r
@@ -1,11 +1,14 @@
 #' This is a function to check the rdata with the archival data has been downloaded.
-#' Git LFS is tricky
+#' After some issues with LFS, I put it in saco...
 
 #' @export
 checkfiles<-function(){
-  eezfilename=system.file("data", "archives.RData", package = "patudo")
-  if (file.info(eezfilename)$size<10000000){
-    file_url="https://github.com/curroabascal/myareas/raw/refs/heads/master/data/archives.RData?download="
-    download.file(file_url, destfile = eezfilename,mode='wb')
+  archivesfilename = system.file("data", "archives.RData", package = "patudo")
+  if (file.info(archivesfilename)$size < 1e+07 | is.na(file.info(archivesfilename)$size)) {
+    options(timeout = max(600, getOption("timeout")))
+    file_url = "https://saco.csic.es/s/7BjiQBtxbAQToRm/download?path=%2F&files=archives.RData"
+    download.file(file_url, destfile = paste0(system.file("data", package = "patudo"),'/archives.RData'), mode = "wb")
   }
 }
+
+  
